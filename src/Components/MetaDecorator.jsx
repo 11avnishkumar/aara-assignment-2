aee4fa5ff315ccfd1fada658c749e7010bb0e0a5
avnish kumar <11avnishkumar@gmail.com>
2023-06-29 17:02:00 +0530

    updated

--- src/Components/MetaDecorator.jsx
+++ src/Components/MetaDecorator.jsx
@@ -9,26 +9,15 @@ const MetaDecorator = ({
 }) => {
   return (
     <Helmet>
-      <title>{titleUrl}</title>
+      <meta charset="UTF-8" />
       <meta name="title" content={titleUrl} />
       <meta name="description" content={descriptionUrl} />
       <meta property="og:title" content={titleUrl} />
       <meta property="og:description" content={descriptionUrl} />
       <meta property="og:image" content={imageUrl} />
       <meta property="og:type" content="website" />
-      <meta
-        property="og:url"
-        content={`https://aara-assignment-2.vercel.app/product_details/${id}`}
-      />
-
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta
-        property="twitter:url"
-        content={`https://aara-assignment-2.vercel.app/product_details/${id}`}
-      />
-      <meta property="twitter:title" content={titleUrl} />
-      <meta property="twitter:description" content={descriptionUrl} />
-      <meta property="twitter:image" content={imageUrl} />
+      <meta property="og:url" content={`${domainUrl} product_details/${id}`} />
+      <title>{titleUrl}</title>
     </Helmet>
   );
 };
