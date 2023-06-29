fbe0c9e72c156ebcfebc0c6c6ece8ac48f10a173
avnish kumar <11avnishkumar@gmail.com>
2023-06-29 16:02:29 +0530

    updated

--- src/Components/SingleProduct.jsx
+++ src/Components/SingleProduct.jsx
@@ -46,14 +46,14 @@ const SingleProduct = () => {
         <meta name="title" content={titleUrl} />
         <meta name="description" content={descriptionUrl} />
 
+        <meta property="og:title" content={titleUrl} />
+        <meta property="og:description" content={descriptionUrl} />
+        <meta property="og:image" content={imageUrl} />
         <meta property="og:type" content="website" />
         <meta
           property="og:url"
           content={`https://aara-assignment-2.vercel.app/product_details/${id}`}
         />
-        <meta property="og:title" content={titleUrl} />
-        <meta property="og:description" content={descriptionUrl} />
-        <meta property="og:image" content={imageUrl} />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta
