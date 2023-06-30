b1feb638767b82d46516c86675db63c2ac37cbbc
avnish kumar <11avnishkumar@gmail.com>
2023-06-30 11:33:46 +0530

    udpated

--- src/Components/MetaDecorator.jsx
+++ src/Components/MetaDecorator.jsx
@@ -18,9 +18,12 @@ const MetaDecorator = ({
       />
       <meta property="og:title" content={titleUrl} />
       <meta property="og:description" content={descriptionUrl} />
-      <meta property="og:image" content={imageUrl} />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content={`${domainUrl} product_details/${id}`} />
+      <meta property="og:image:type" content="image/jpg" />
+      <meta property="og:image" content={imageUrl} />
+      <meta property="og:image:width" content="300" />
+      <meta property="og:image:height" content="300" />
+      <meta property="og:url" content={`${domainUrl}/product_details/${id}`} />
       <title>{titleUrl}</title>
     </Helmet>
   );
